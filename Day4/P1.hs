8c7abb26d54e87d38d96d1643a2405b8907c6096
Divy <divy1211.dc@gmail.com>
2023-12-07 02:45:02 +0800

    Solved Day 4

---
+++ Day4/P1.hs
@@ -0,0 +1,37 @@
+module Day4.P1 (solve, parseCards) where
+
+import Data.Char (isDigit, digitToInt)
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+
+parseCards :: String -> (Set Int, Set Int)
+parseCards = (\(winNums, myNums, _, _) -> (winNums, myNums)) . foldl (\(winNums, myNums, isMyNum, num) c ->
+    let (num', winNums', myNums') = case c of
+            x | isDigit x           -> (num * 10 + digitToInt c, winNums, myNums)
+              | num /= 0 && isMyNum -> (0, winNums, Set.insert num myNums)
+              | num /= 0            -> (0, Set.insert num winNums, myNums)
+            _                       -> (0, winNums, myNums)
+        in
+    (winNums', myNums', isMyNum || c == '|', num')
+    ) (Set.empty, Set.empty, False, 0)
+
+calcScore :: String -> Int
+calcScore str =
+    let (winNums, myNums) = parseCards str in
+    let commonNums = Set.intersection winNums myNums in
+    let numCommonNums = length commonNums in
+    
+    case numCommonNums of
+        0 -> 0
+        x -> 2 ^ (x - 1)
+
+solve :: IO ()
+solve = do
+    inp <- readFile "Day4/inp.txt"
+    let allLines = lines inp
+    let paddedLines = map ((++ " ") . dropWhile (/= ':')) allLines
+
+    let cardScores = map calcScore paddedLines
+
+    print (sum cardScores)
---
+++ Day4/P2.hs
@@ -0,0 +1,32 @@
+module Day4.P2 (solve) where
+
+import Day4.P1 (parseCards)
+import qualified Data.Set as Set
+
+
+calcNumMatches :: String -> Int
+calcNumMatches str =
+    let (winNums, myNums) = parseCards str in
+    let commonNums = Set.intersection winNums myNums in
+    length commonNums
+
+updateCounts :: Int -> [(Int, Int)] -> [(Int, Int)]
+updateCounts count = map (\(cardMatches, cardCount) -> (cardMatches, cardCount + count))
+
+totalCards :: [(Int, Int)] -> Int
+totalCards ((matches, count) : rest) =
+    case splitAt matches rest of
+        ([], [])             -> count
+        ([], rest')          -> count + totalCards rest'
+        (cardsToCopy, [])    -> count + totalCards (updateCounts count cardsToCopy)
+        (cardsToCopy, rest') -> count + totalCards (updateCounts count cardsToCopy ++ rest')
+
+solve :: IO ()
+solve = do
+    inp <- readFile "Day4/inp.txt"
+    let allLines = lines inp
+    let paddedLines = map ((++ " ") . dropWhile (/= ':')) allLines
+
+    let numMatchesCards = map calcNumMatches paddedLines
+
+    print (totalCards (zip numMatchesCards (repeat 1)))
